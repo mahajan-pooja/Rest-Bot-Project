3f4bdd9cad46898bb9dff3f608a83744c2067315
marcusLau <39976569+marcusLau@users.noreply.github.com>
2018-08-08 20:52:52 -0700

    created TheaterServiceImpl

--- demo/src/main/java/com/example/demo/service/TheaterServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/TheaterServiceImpl.java
@@ -2,41 +2,33 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 import com.example.demo.bean.Theater;
-import com.example.demo.dao.TheaterDao; // imported here..?
-
-import java.util.List;
+import com.example.demo.dao.TheaterDao; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Service("theaterService")
-public class TheaterServiceImpl implements TheaterService {
+public class TheaterServiceImpl {
 	
 	@Autowired
-	private TheaterDao dao; // Dont know why this is giving me an error when i imported it correctly above
+	private TheaterDao theaterDao; 
 
-	@Override
-	public List<Theater> getTheaters() {
+	public Theater getTheater() {
 		// TODO Auto-generated method stub
-		return null;
+		return theaterDao.getTheater();
 	}
 
-	@Override
 	public int deleteTheaters(Long theatreId) {
-		// TODO Auto-generated method stub
-		return 0;
+		return theaterDao.deleteTheaters(theatreId);
 	}
 
-	@Override
-	public int updateTheaters(Theater theatre) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int updateTheaters(Theater theaterId) {
+		return theaterDao.updateTheaters(theaterId);
 	}
 
-	@Override
-	public int createTheaters(Theater theatre) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+//	@Override
+//	public int createTheaters(Theater theatre) {
+//		return theaterDao.createTheaters(theatre);
+//	}
 
 }
